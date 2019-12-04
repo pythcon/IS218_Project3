8fbcb549083faddfb65f14af12e461cb4a3b4614
Todd Murphy <tam1998@comcast.net>
2019-12-04 18:39:19 -0500

    initial commit

---
+++ view/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <p class="copyright">
+        &copy; <?php echo date("Y"); ?> Todd Murphy
+    </p>
+</footer>
+</body>
+</html>
---
+++ view/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    <title>IS218 Project 3</title>
+    <link rel="stylesheet" type="text/css"
+          href="/book_apps/ch05_guitar_shop/main.css">
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>IS 218 Project 3</h1></header>
