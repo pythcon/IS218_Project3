1361e085d197627b40a2d3e94b589288e55aa271
Todd Murphy <tam1998@comcast.net>
2019-12-10 22:38:03 -0500

    added placeholders

--- views/registration.php
+++ views/registration.php
@@ -5,27 +5,27 @@
         
         <div class="form-group">
             <label for="firstName">First Name</label>
-            <input type="text" class="form-control" name="firstName" id="firstName">
+            <input type="text" class="form-control" name="firstName" id="firstName" placeholder="Johnny">
         </div>
 
         <div class="form-group">
             <label for="lastName">Last Name</label>
-            <input type="text" class="form-control" name="lastName" id="lastName">
+            <input type="text" class="form-control" name="lastName" id="lastName" placeholder="Appleseed">
         </div>
         
         <div class="form-group">
             <label for="birthday">Birthday</label>
-            <input type="text" class="form-control" name="birthday" id="birthday">
+            <input type="text" class="form-control" name="birthday" id="birthday" placeholder="1970/01/30">
         </div>
 
         <div class="form-group">
             <label for="email">Email Address</label>
-            <input type="email" class="form-control" name="email" id="email">
+            <input type="email" class="form-control" name="email" id="email" placeholder="JAppleseed@example.com">
         </div>
 
         <div class="form-group">
             <label for="password">Password</label>
-            <input type="password" class="form-control" name="password" id="password">
+            <input type="password" class="form-control" name="password" id="password" placeholder="Password">
         </div>
 
         <button type="submit" class="btn btn-primary">Submit</button>
