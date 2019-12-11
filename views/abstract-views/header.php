f3652a623a9390225365bde6d5df45e467d8268e
Todd Murphy <tam1998@comcast.net>
2019-12-10 20:45:33 -0500

    fixed title and css styling

--- views/abstract-views/header.php
+++ views/abstract-views/header.php
@@ -2,7 +2,7 @@
 <html>
 <!-- the head section -->
 <head>
-    <title>IS218 MVC Practice</title>
+    <title>IS218 Project 3</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="main.css" rel="stylesheet">
 </head>
