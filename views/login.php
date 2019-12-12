7d1a859f6b8d81e7f140df578c285b541af834eb
Todd Murphy <tam1998@comcast.net>
2019-12-12 00:41:03 -0500

    fixed autocomplete and some styling bugs

--- views/login.php
+++ views/login.php
@@ -5,12 +5,12 @@
 
         <div class="form-group">
             <label for="email">Email Address</label>
-            <input type="email" class="form-control" name="email" id="email">
+            <input type="email" class="form-control" name="email" id="email" autocomplete="off">
         </div>
 
         <div class="form-group">
             <label for="password">Password</label>
-            <input type="password" class="form-control" name="password" id="password">
+            <input type="password" class="form-control" name="password" id="password" autocomplete="off">
         </div>
 
         <button type="submit" class="btn btn-primary">Submit</button>
--- views/registration.php
+++ views/registration.php
@@ -5,27 +5,27 @@
         
         <div class="form-group">
             <label for="firstName">First Name</label>
-            <input type="text" class="form-control" name="firstName" id="firstName" placeholder="Johnny">
+            <input type="text" class="form-control" name="firstName" id="firstName" placeholder="Johnny" autocomplete="off">
         </div>
 
         <div class="form-group">
             <label for="lastName">Last Name</label>
-            <input type="text" class="form-control" name="lastName" id="lastName" placeholder="Appleseed">
+            <input type="text" class="form-control" name="lastName" id="lastName" placeholder="Appleseed" autocomplete="off">
         </div>
         
         <div class="form-group">
             <label for="birthday">Birthday</label>
-            <input type="text" class="form-control" name="birthday" id="birthday" placeholder="1970/01/30">
+            <input type="text" class="form-control" name="birthday" id="birthday" placeholder="1970/01/30" autocomplete="off">
         </div>
 
         <div class="form-group">
             <label for="email">Email Address</label>
-            <input type="email" class="form-control" name="email" id="email" placeholder="JAppleseed@example.com">
+            <input type="email" class="form-control" name="email" id="email" placeholder="JAppleseed@example.com" autocomplete="off">
         </div>
 
         <div class="form-group">
             <label for="password">Password</label>
-            <input type="password" class="form-control" name="password" id="password" placeholder="Password">
+            <input type="password" class="form-control" name="password" id="password" placeholder="Password" autocomplete="off">
         </div>
 
         <button type="submit" class="btn btn-primary">Submit</button>
